80160fecacfcd796810cdd3786a17698800b7db4
Justin <justinlillico@gmail.com>
2017-07-28 09:56:26 +0100

    Another test

--- laptoptest.py
+++ laptoptest.py
@@ -1,2 +1,5 @@
-while (True):
-	print ("cats")
+
+person = input ("Enter your name: ")
+yob = int(input ("Enter your year of birth: "))
+
+print ("Well well well, your name is ", person, " and you are ", 2017-yob, " years old!")
