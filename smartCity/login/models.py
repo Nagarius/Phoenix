550f9ac8e77ba22dc7a04c4c1eccbe16cd2dc33b
Nagarius <pooper2010@live.com.au>
2017-08-27 03:28:00 +1000

    Update models for userInfo
    Added additional tables userType

--- smartCity/login/models.py
+++ smartCity/login/models.py
@@ -3,6 +3,7 @@ from django.db import models
 # Create your models here.
 class userInfo(models.Model):
     userID = models.IntegerField(max_length=10)
+    userTypeID = models.ForeignKey(userType, on_delete=models.CASCADE)
     username = models.CharField(max_length=50)
     password = models.CharField(max_length=30)
     fname = models.CharField(max_length=50)
@@ -11,3 +12,15 @@ class userInfo(models.Model):
     email = models.CharField(50)
     contactNumber = models.IntegerField(max_length=50)
     address = models.CharField(50)
+
+class userType(models.Model):
+    typeName = models.CharField(max_length=50)
+    typeDesc = models.TextField()
+    userTypeID = models.CharField(max_length=1, choices=USER_TYPE)
+
+    USER_TYPE = (
+        ('T', 'Tourist'),
+        ('B', 'Business'),
+        ('S', 'Student'),
+        ('A', 'Administrator'),
+    )
