1cdb55bc72474c86ab09fe232aeca95cbb4d789e
Nagarius <pooper2010@live.com.au>
2017-08-27 03:08:53 +1000

    init models for userInfo
    Started building a database model for the login.

--- smartCity/login/models.py
+++ smartCity/login/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class userInfo(models.Model):
+    userID = models.IntegerField(max_length=10)
+    username = models.CharField(max_length=50)
+    password = models.CharField(max_length=30)
+    fname = models.CharField(max_length=50)
+    lname = models.CharField(max_length=50)
+    dob = models.DateField()
+    email = models.CharField(50)
+    contactNumber = models.IntegerField(max_length=50)
+    address = models.CharField(50)
