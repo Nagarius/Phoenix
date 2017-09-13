f2ab4b7ed3c529994d3e5ef6a7ba58e8e6fbcb7b
Justin <justinlillico@gmail.com>
2017-09-13 12:40:36 +1000

    Updated login, remembers

--- smartCity/login/admin.py
+++ smartCity/login/admin.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
-from .models import UserType, UserInfo
 
 # Register your models here.
 
-admin.site.register(UserType, )
-admin.site.register(UserInfo)
+#admin.site.register(UserType, )
+#admin.site.register(UserInfo)
--- smartCity/smartCity/urls.py
+++ smartCity/smartCity/urls.py
@@ -15,10 +15,12 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
     url(r'^$', views.index),
-    url(r'^login/', include('login.urls')),
+    url(r'^login/', auth_views.login, name='login'),
+    url(r'^logout/', auth_views.login, name='logout'),
     url(r'^admin/', admin.site.urls),
 ]
