24547bcf905c3af1916b2dedfdebc96e48f2fdf3
Justin <justinlillico@gmail.com>
2017-10-14 15:14:54 +1000

    more function to the search

--- smartCity/smartCity/views.py
+++ smartCity/smartCity/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse, HttpRequest
 from django.conf.urls import url
 from accounts import models
+import re
 
 # Create your views here.
 
@@ -11,13 +12,21 @@ def searchResults(request):
 
     # Perhaps it would be better to check the search against a dictionary here before passing it to render. That way,
     # can determine what to render and have another page for a fail or something.
+    searchTerm = request.session['searchTerm']
 
-    context = {
-        "searchTerm": request.session['searchTerm'],
-    }
+    possibleSearches = ['colleges', 'college', 'libraries', 'library', 'industries', 'industry', 'hotels', 'hotel', 'parks', 'park', 'zoos', 'zoo',
+                        'museums', 'museum', 'restaurants', 'restaurant', 'malls', 'mall']
 
-    return render(request, 'searchResults.html', context)
 
+    for word in possibleSearches:
+        if word in str.lower(searchTerm):
+
+            context = {
+                "searchTerm": word,
+            }
+
+            return render(request, 'searchResults.html', context)
+    return HttpResponse("<html><body>No results</body></html>")
 
 def index(request):
 
